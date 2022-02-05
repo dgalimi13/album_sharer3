276280eb6222ef100289842ba32df8d7365f3d87
danny galimi <dannygalimi13@aim.com>
2022-02-05 15:47:46 -0500

    added create and album params actions to albums controller

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -4,5 +4,20 @@ class Api::V1::AlbumsController < ApplicationController
         albums = Album.all
         render json: AlbumSerializer.new(albums)
     end 
-    
+
+    def create
+        album = Album.new(album_params)
+        if album.save
+            render json: album, status: :accepted
+        else
+            render json: { errors: album.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
+
+    private
+
+    def album_params
+        params.require(:album).permit(:name, :artist, :description, :genre_id)
+    end 
+
 end
