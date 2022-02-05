9f7dce8cdd079eae79a9190c5923d0065d083f16
danny galimi <dannygalimi13@aim.com>
2022-02-05 15:31:40 -0500

    generated Album model and migrations

---
+++ db/migrate/20220205203055_create_albums.rb
@@ -0,0 +1,12 @@
+class CreateAlbums < ActiveRecord::Migration[6.1]
+  def change
+    create_table :albums do |t|
+      t.string :name
+      t.string :artist
+      t.string :description
+      t.references :genre, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+rock = Genre.create(name: "Rock")
+rap = Genre.create(name: "Rap")
+country = Genre.create(name: "Country")
+
+Album.create(name: "Three Cheers for Sweet Revenge", artist: "My Chemical Romance", description: "Third Studio Album released in 2004", genre_id: rock.id)
