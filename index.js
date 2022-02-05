c1bac01b8251abbb11e1de0ef4d905d3be4e9c28
danny galimi <dannygalimi13@aim.com>
2022-02-05 16:36:57 -0500

    added getAlbums funtion to index.js and called in inside DOMContentLoaded event listener

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const endPoint = "http://127.0.0.1:3000/api/v1/albums"
 
 document.addEventListener('DOMContentLoaded', () => {
+    getAlbums()
+})
+
+function getAlbums() {
     fetch(endPoint)
     .then(response => response.json())
     .then(albums => {
-        console.log(albums);
-  })
-})
+        console.log(albums)
+    })
+}
